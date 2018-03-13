95308f5bfcac2394c7a2ddba433e42d0765f5772
ItsDizzy <itsdizzy40@gmail.com>
2018-03-13 19:40:18 +0100

    Finished the webhook and added payload verification

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,37 +1,63 @@
 const Boom = require('boom');
 const got = require('got');
+const Joi = require('joi');
 const config = require('../config.json');
 
+function getStatus(data) {
+  const status = {
+    color: 3779158,
+    time: data.finished_at,
+    message: data.result_message
+  }
+
+  if (status.message === null) {
+    status.message = data.status_message;
+  }
+
+  switch (status.message) {
+    case "Pending":
+      status.color = 15588927;
+      status.time = data.started_at;
+      break;
+    case "Passed":
+    case "Fixed":
+      status.color = 3779158;
+      break;
+    case "Broken":
+    case "Failed":
+    case "Still Failing":
+      status.color = 14370117;
+      break;
+    case "Canceled":
+      status.color = 10329501;
+  }
+
+  return status;
+}
 
 module.exports = (key) => ({
-  path: '/webhook/{repository}',
+  path: '/webhook',
   method: 'POST',
   handler: async (req) => {
-    // if(!req.headers.signature) {
-    //   return Boom.badRequest('No signature found on request');
-    // }
-
-    if(!req.payload) {
-      return Boom.badRequest('No data submitted');
+    if(!key.verify(req.payload), req.headers.signature, 'base64', 'base64') {
+      return Boom.unauthorized('Signed payload does not match signature');
     }
 
-    // if(!key.verify(req.payload), req.headers.signature, 'base64', 'base64') {
-    //   return Boom.unauthorized('Signed payload does not match signature');
-    // }
+    const data = req.payload;
+    const status = getStatus(data);
 
-    const foundEntry = config.repositories.find(repository => repository.name === req.params.repository)
+    const foundEntry = config.repositories.find(repository => repository.name === data.repository.name)
 
     if(!foundEntry) {
-      return Boom.unauthorized('Repository not registered!');
+      return Boom.badRequest('Repository not registered!');
     }
-    
-    //console.log(req.payload);
 
-    const { number, author_name, build_url, repository, branch, commit, message, finished_at } = JSON.parse(req.payload);
+    if(!~foundEntry.branches.indexOf(data.branch)) {
+      return Boom.badRequest('Branch not registered!');
+    }
 
-    const status = {
-      color: 3779158,
-      message: 'Success'
+    if(!~foundEntry.hooks.indexOf(status.message)) {
+      return Boom.badRequest('Hook not registered!');
     }
 
     const payload = {
@@ -40,28 +66,55 @@ module.exports = (key) => ({
       embeds: [{
         color: status.color,
         author: {
-          name: `Build #${number} ${status.message} - ${author_name}`,
-          url: build_url
+          name: `Build #${data.number} ${status.message} - ${data.author_name}`,
+          url: data.build_url
         },
-        title: `[${repository.name}:${branch}]`,
-        url: `https://github.com/${repository.owner_name}/${repository.name}`,
-        description: `[${commit.substring(0, 7)}](https://github.com/${repository.owner_name}/${repository.name}/commit/${commit}) ${message}`,
-        timestamp: finished_at
+        title: `[${data.repository.name}:${data.branch}]`,
+        url: `https://github.com/${data.repository.owner_name}/${data.repository.name}`,
+        description: `[${data.commit.substring(0, 7)}](https://github.com/${data.repository.owner_name}/${data.repository.name}/commit/${data.commit}) ${data.message}`,
+        timestamp: status.time
       }]
     }
 
-    console.log(JSON.stringify(payload));
     try {
       await got.post(foundEntry.webhook, {
         body: payload,
         json: true
       })
     } catch(e) {
-      console.log(e);
+      return Boom.internal(e.message);
     }
 
     return {
       ok: true
     };
+  },
+
+  options: {
+    validate: {
+      options: {
+        allowUnknown: true
+      },
+      headers: {
+        signature: Joi.string()
+      },
+      payload: Joi.object({
+        number: Joi.number(),
+        status_message: Joi.string(),
+        result_message: Joi.string().optional(),
+        started_at: Joi.string(),
+        finished_at: Joi.string(),
+        build_url: Joi.string(),
+        commit: Joi.string(),
+        branch: Joi.string(),
+        message: Joi.string(),
+        author_name: Joi.string(),
+        repository: Joi.object({
+          id: Joi.number(),
+          name: Joi.string(),
+          owner_name: Joi.string(),
+        })
+      })
+    }
   }
 });
